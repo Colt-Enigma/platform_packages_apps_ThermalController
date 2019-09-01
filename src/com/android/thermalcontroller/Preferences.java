silent27121984/packages_apps_ThermalController

Implement per-app thermal control [3/3]
Change-Id: I4a24474797fabf9b5ed4b5f3b297f053da4e4c75

src/org/pixelexperience/thermalcontroller/Preferences.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.thermalcontroller;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+import java.util.stream.IntStream;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class Preferences {
+    private static String PREFS_NAME = "ThermalController_Settings";
+    public static void setProfileId(Context context, String packageName, int profileId){
+        if (TextUtils.isEmpty(packageName) || !IntStream.of(ThermalProfiles.supportedProfiles).anyMatch(x -> x == profileId)){
+            return;
+        }
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putInt(packageName, profileId);
+        editor.commit();
+    }
+    public static int getProfileId(Context context, String packageName){
+        if (TextUtils.isEmpty(packageName)){
+            return ThermalProfiles.MODE_DEFAULT;
+        }
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        return prefs.getInt(packageName, ThermalProfiles.getDefaultProfileId(packageName));
+    }
+}

src/org/pixelexperience/thermalcontroller/PreferencesProvider.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.thermalcontroller;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import static com.android.internal.util.custom.thermal.ThermalController.*;
+
+public class PreferencesProvider extends ContentProvider {
+    private static final int PREFERENCES = 1;
+    private static final UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, "preferences/*", PREFERENCES);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        if (uriMatcher.match(uri) != PREFERENCES) {
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+        String packageName = uri.getPathSegments().get(1);
+        if (TextUtils.isEmpty(packageName)){
+            return null;
+        }
+        int profile = (int) values.get(COLUMN_PROFILE);
+        Preferences.setProfileId(getContext(), packageName, profile);
+        return Uri.parse(CONTENT_URI + "/" + packageName);
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        if (uriMatcher.match(uri) != PREFERENCES) {
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+        String packageName = uri.getPathSegments().get(1);
+        if (TextUtils.isEmpty(packageName)){
+            return null;
+        }
+        MatrixCursor result = new MatrixCursor(PROJECTION_DEFAULT);
+        result.newRow().add(COLUMN_PROFILE, Preferences.getProfileId(getContext(), packageName));
+        return result;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        return 0;
+    }
+
+}

src/org/pixelexperience/thermalcontroller/Receiver.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.thermalcontroller;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.text.TextUtils;
+
+import com.android.internal.util.custom.thermal.ThermalController;
+
+public class Receiver extends BroadcastReceiver {
+    private static final String TAG = "ThermalController:Receiver";
+    private static final boolean DEBUG = false;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null && intent.getAction().equals(ThermalController.ACTIVE_PACKAGE_CHANGED_ACTION)) {
+            if (DEBUG) Log.d(TAG, "Received " + ThermalController.ACTIVE_PACKAGE_CHANGED_ACTION);
+            if (intent.getExtras() != null) {
+                String packageName = intent.getExtras().getString(ThermalController.ACTIVE_PACKAGE_CHANGED_EXTRA);
+                int profile = 0;
+                if (packageName != null && !TextUtils.isEmpty(packageName)) {
+                    if (DEBUG) Log.d(TAG, "Received " + packageName);
+                    profile = Preferences.getProfileId(context, packageName);
+                    if (packageName.equals("com.tencent.ig") && profile == ThermalProfiles.MODE_GAME) {
+                        profile = ThermalProfiles.MODE_PUBG;
+                    }
+                }
+                ThermalProfiles.writeProfile(profile);
+            }
+        }
+    }
+}

src/org/pixelexperience/thermalcontroller/ThermalProfiles.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.pixelexperience.thermalcontroller;
+
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class ThermalProfiles {
+    public static final int MODE_DEFAULT = 0;
+    public static final int MODE_DIALER = 8;
+    public static final int MODE_GAME = 9;
+    public static final int MODE_PERFORMANCE = 10;
+    public static final int MODE_BROWSER = 11;
+    public static final int MODE_CAMERA = 12;
+    public static final int MODE_PUBG = 13;
+    public static final int MODE_VIDEO = 14;
+
+    public static final int supportedProfiles[] = {MODE_DEFAULT, MODE_DIALER, MODE_GAME,
+        MODE_PERFORMANCE, MODE_BROWSER, MODE_CAMERA, MODE_PUBG, MODE_VIDEO};
+
+    private static final String TAG = "ThermalController:ThermalProfiles";
+
+    public static int getDefaultProfileId(String packageName) {
+        switch (packageName) {
+            case "com.android.dialer":
+            case "com.google.android.dialer":
+                return MODE_DIALER;
+            case "com.antutu.ABenchMark":
+            case "com.futuremark.dmandroid.application":
+            case "com.primatelabs.geekbench":
+                return MODE_PERFORMANCE;
+            case "com.tencent.ig":
+            case "com.dts.freefireth":
+            case "com.epicgames.fortnite":
+            case "com.gameloft.android.ANMP.GloftA8HM":
+            case "com.gameloft.android.ANMP.GloftA9HM":
+                return MODE_GAME;
+            case "com.google.android.youtube":
+            case "com.netflix.mediaclient":
+            case "com.google.android.videos":
+            case "com.amazon.avod.thirdpartyclient":
+            case "com.google.android.apps.youtube.kids":
+                return MODE_VIDEO;
+            case "org.lineageos.jelly":
+            case "com.android.chrome":
+            case "com.UCMobile.intl":
+                return MODE_BROWSER;
+            case "org.codeaurora.snapcam":
+            case "com.android.camera":
+            case "com.android.gallery3d":
+            case "com.google.android.apps.photos":
+            case "com.google.android.GoogleCamera":
+                return MODE_CAMERA;
+            default:
+                return MODE_DEFAULT;
+        }
+    }
+
+    public static void writeProfile(int profileId) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("/sys/class/thermal/thermal_message/sconfig"))) {
+            writer.write(Integer.toString(profileId));
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to write profile", e);
+        }
+    }
+}

Colt-Enigma/platform_packages_apps_ThermalController

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

src/com/android/thermalcontroller/Preferences.java
@@ -1,0 +1,43 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.thermalcontroller;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.text.TextUtils;
+import java.util.stream.IntStream;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class Preferences {
+    private static String PREFS_NAME = "ThermalController_Settings";
+    public static void setProfileId(Context context, String packageName, int profileId){
+        if (TextUtils.isEmpty(packageName) || !IntStream.of(ThermalProfiles.supportedProfiles).anyMatch(x -> x == profileId)){
+            return;
+        }
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putInt(packageName, profileId);
+        editor.commit();
+    }
+    public static int getProfileId(Context context, String packageName){
+        if (TextUtils.isEmpty(packageName)){
+            return ThermalProfiles.MODE_DEFAULT;
+        }
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        return prefs.getInt(packageName, ThermalProfiles.getDefaultProfileId(packageName));
+    }
+}

src/com/android/thermalcontroller/PreferencesProvider.java
@@ -1,0 +1,88 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.thermalcontroller;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.text.TextUtils;
+
+import static com.android.internal.util.thermal.ThermalController.*;
+
+public class PreferencesProvider extends ContentProvider {
+    private static final int PREFERENCES = 1;
+    private static final UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI(AUTHORITY, "preferences/*", PREFERENCES);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        if (uriMatcher.match(uri) != PREFERENCES) {
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+        String packageName = uri.getPathSegments().get(1);
+        if (TextUtils.isEmpty(packageName)){
+            return null;
+        }
+        int profile = (int) values.get(COLUMN_PROFILE);
+        Preferences.setProfileId(getContext(), packageName, profile);
+        return Uri.parse(CONTENT_URI + "/" + packageName);
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        if (uriMatcher.match(uri) != PREFERENCES) {
+            throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
+        String packageName = uri.getPathSegments().get(1);
+        if (TextUtils.isEmpty(packageName)){
+            return null;
+        }
+        MatrixCursor result = new MatrixCursor(PROJECTION_DEFAULT);
+        result.newRow().add(COLUMN_PROFILE, Preferences.getProfileId(getContext(), packageName));
+        return result;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        return 0;
+    }
+
+}

src/com/android/thermalcontroller/Receiver.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.thermalcontroller;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.text.TextUtils;
+
+import com.android.internal.util.thermal.ThermalController;
+
+public class Receiver extends BroadcastReceiver {
+    private static final String TAG = "ThermalController:Receiver";
+    private static final boolean DEBUG = false;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null && intent.getAction().equals(ThermalController.ACTIVE_PACKAGE_CHANGED_ACTION)) {
+            if (DEBUG) Log.d(TAG, "Received " + ThermalController.ACTIVE_PACKAGE_CHANGED_ACTION);
+            if (intent.getExtras() != null) {
+                String packageName = intent.getExtras().getString(ThermalController.ACTIVE_PACKAGE_CHANGED_EXTRA);
+                int profile = 0;
+                if (packageName != null && !TextUtils.isEmpty(packageName)) {
+                    if (DEBUG) Log.d(TAG, "Received " + packageName);
+                    profile = Preferences.getProfileId(context, packageName);
+                    if (packageName.equals("com.tencent.ig") && profile == ThermalProfiles.MODE_GAME) {
+                        profile = ThermalProfiles.MODE_PUBG;
+                    }
+                }
+                ThermalProfiles.writeProfile(profile);
+            }
+        }
+    }
+}

src/com/android/thermalcontroller/ThermalProfiles.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright (c) 2019 The PixelExperience Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.thermalcontroller;
+
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class ThermalProfiles {
+    public static final int MODE_DEFAULT = 0;
+    public static final int MODE_DIALER = 8;
+    public static final int MODE_GAME = 9;
+    public static final int MODE_PERFORMANCE = 10;
+    public static final int MODE_BROWSER = 11;
+    public static final int MODE_CAMERA = 12;
+    public static final int MODE_PUBG = 13;
+    public static final int MODE_VIDEO = 14;
+
+    public static final int supportedProfiles[] = {MODE_DEFAULT, MODE_DIALER, MODE_GAME,
+        MODE_PERFORMANCE, MODE_BROWSER, MODE_CAMERA, MODE_PUBG, MODE_VIDEO};
+
+    private static final String TAG = "ThermalController:ThermalProfiles";
+
+    public static int getDefaultProfileId(String packageName) {
+        switch (packageName) {
+            case "com.android.dialer":
+            case "com.google.android.dialer":
+                return MODE_DIALER;
+            case "com.antutu.ABenchMark":
+            case "com.futuremark.dmandroid.application":
+            case "com.primatelabs.geekbench":
+                return MODE_PERFORMANCE;
+            case "com.tencent.ig":
+            case "com.dts.freefireth":
+            case "com.epicgames.fortnite":
+            case "com.gameloft.android.ANMP.GloftA8HM":
+            case "com.gameloft.android.ANMP.GloftA9HM":
+                return MODE_GAME;
+            case "com.google.android.youtube":
+            case "com.netflix.mediaclient":
+            case "com.google.android.videos":
+            case "com.amazon.avod.thirdpartyclient":
+            case "com.google.android.apps.youtube.kids":
+                return MODE_VIDEO;
+            case "org.lineageos.jelly":
+            case "com.android.chrome":
+            case "com.UCMobile.intl":
+                return MODE_BROWSER;
+            case "org.codeaurora.snapcam":
+            case "com.android.camera":
+            case "com.android.gallery3d":
+            case "com.google.android.apps.photos":
+            case "com.google.android.GoogleCamera":
+                return MODE_CAMERA;
+            default:
+                return MODE_DEFAULT;
+        }
+    }
+
+    public static void writeProfile(int profileId) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("/sys/class/thermal/thermal_message/sconfig"))) {
+            writer.write(Integer.toString(profileId));
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to write profile", e);
+        }
+    }
+}